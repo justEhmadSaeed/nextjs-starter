ed8578c24f10e22e45614c11fb35312f37c8ad3d
Ehmad <justehmadsaeed@gmail.com>
2021-09-03 15:57:28 +0500

    📦 NEW: Auto-redirect user from 404 to homepage

--- pages/404.js
+++ pages/404.js
@@ -1,5 +1,15 @@
 import Link from 'next/link';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 const NotFoundPage = () => {
+    const router = useRouter();
+    
+    useEffect(() => {
+        // Redirect the user after 3 seconds
+		setTimeout(() => {
+			router.push('/');
+		}, 3000);
+	}, []);
 	return (
 		<div className='not-found'>
 			<h1>Page Not Found!</h1>
